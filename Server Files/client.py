94a41a615808488f0de26700cb355898db868464
Diego Alejandro Restrepo Sanchez <diegorestrepo68@utp.edu.co>
2019-04-19 11:11:12 -0500

    add Server Files is Complete, but without Error Manager

--- Server Files/client.py
+++ Server Files/client.py
@@ -1,6 +1,69 @@
 import zmq
+import hashlib
+import os
+
 
 context = zmq.Context()
-socket = context.socket(zmq.REQ) # REQ because I going to request
+socket = context.socket(zmq.REQ)                # REQ because I going to request
 socket.connect("tcp://127.0.0.1:3000")
-print("Client ON")
+print("<option> File")
+print("submit pathFile")
+print("download File")
+print("add hash")
+sizePart = 1024*1024*1024*10  # 10MB
+sizeBuf = 65536
+Files = {}
+
+def submit(path):
+    # Creo el hash con el cual va a quedar en el servidor
+    with open(path, 'rb') as f :
+        sha1 = hashlib.sha1()
+        while True:
+            data = f.read(sizeBuf)
+            if not data :
+                break
+            sha1.update(data)
+
+    # Save NameFile with your respect hash 
+    Files[os.path.basename(path)] = sha1.hexdigest()
+
+    # Submit File in the server
+    name = sha1.hexdigest().encode()                            # File's hash
+    socket.send_multipart([b"create", name])                    # Create File In server
+    ans = socket.recv()
+    with open(path, 'rb') as f :
+        while True:
+            data = f.read(sizeBuf)
+            if not data :
+                break
+            socket.send_multipart([b'submit', name, data])      # Send Data
+            #print(data)
+            ans = socket.recv()                                 # The server forever Reply "OK"
+    print(sha1.hexdigest())
+    
+def download(name):
+    with open(name, "wb") as f:
+        socket.send_multipart([b"download", Files[name].encode()])
+        while True :
+            ans = socket.recv()
+            if b"Finish" == ans:
+                print("Done")
+                break
+            f.write(ans)
+            socket.send(b"OK")
+
+
+def add(value):
+    print("Escribe el nombre del archivo con extension")
+    nombre = input()
+    Files[nombre] = value
+
+switcher = { "submit" : submit, "download" : download, "add" : add}
+
+while True:
+    valor = input().split()
+    if len(valor) != 2:
+        print("No Esta Bien Escrito")
+    else:
+        if valor[0] in switcher:
+            switcher[valor[0]](valor[1])
--- Server Files/server.py
+++ Server Files/server.py
@@ -1,7 +1,44 @@
 import zmq
+import os
 
+sizePart = 1024*1024*1024*10  # 10MB
 context = zmq.Context()
 socket = context.socket(zmq.REP) # REP because is Reply
 socket.bind("tcp://*:3000")
 
+
+def create(name) :
+    name = name[1].decode()
+    with open(name, "a") as f:
+        pass
+    os.remove(name)
+    with open(name, "a") as f:
+        pass
+    socket.send(b"OK")
+
+def submit(data) :
+    name = data[1].decode()
+    with open(name, "ab") as f:
+        f.write(data[2])
+    socket.send(b"OK")
+
+def download(name) :
+    name = name[1].decode()
+    with open(name, "rb") as f:
+        while True:
+            data = f.read(sizePart)
+            if not data:
+                socket.send(b"Finish")
+                break
+            socket.send(data)
+            socket.recv()
+
+switcher = {"create": create, "submit" : submit, "download": download}
+
 print("Server ON")
+
+while True :
+    operation = socket.recv_multipart()
+    operation[0] = operation[0].decode()
+    print(operation[0])
+    switcher[operation[0]](operation)
