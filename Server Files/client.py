4d35d63f01a246bd24b8eae3a93987ace0521ed0
Diego Alejandro Restrepo Sanchez <diegorestrepo68@utp.edu.co>
2019-04-23 23:48:01 -0500

    added Trie and resolve the problems with download

--- Server Files/client.py
+++ Server Files/client.py
@@ -11,10 +11,10 @@ print("<option> File")
 print("submit pathFile")
 print("download File")
 print("add hash")
-sizePart = 1024*1024*1024*10  # 10MB
-sizeBuf = 65536
+sizeBuf = 1024*1024*10  # 10MB
 Files = {}
 
+# This is to submit file to server
 def submit(path):
     # Creo el hash con el cual va a quedar en el servidor
     with open(path, 'rb') as f :
@@ -33,28 +33,31 @@ def submit(path):
     name = sha1.hexdigest().encode()                            # File's hash
     socket.send_multipart([b"create", name])                    # Create File In server
     ans = socket.recv()
-    with open(path, 'rb') as f :
-        while True:
-            data = f.read(sizeBuf)
-            if not data :
-                break
-            socket.send_multipart([b'submit', name, data])      # Send Data
-            #print(data)
-            ans = socket.recv()                                 # The server forever Reply "OK"
+    if b"false" == ans:
+        with open(path, 'rb') as f :
+            while True:
+                data = f.read(sizeBuf)
+                if not data :
+                    break
+                socket.send_multipart([b'submit', name, data])      # Send Data
+                #print(data)
+                ans = socket.recv()                                 # The server forever Reply "OK"
     print(sha1.hexdigest())
-    
+
+# This for Download Files
 def download(name):
     with open(name, "wb") as f:
-        socket.send_multipart([b"download", Files[name].encode()])
+        pos = 0
         while True :
+            socket.send_multipart([b"download", Files[name].encode(), str(pos).encode()])
             ans = socket.recv()
             if b"Finish" == ans:
                 print("Done")
                 break
             f.write(ans)
-            socket.send(b"OK")
+            pos += sizeBuf
 
-
+# This is for share 
 def add(value):
     print("Escribe el nombre del archivo con extension")
     nombre = input()
@@ -64,15 +67,18 @@ def add(value):
 
 switcher = { "submit" : submit, "download" : download, "add" : add}
 
+# create file JSON for save information
 with open("datos.json", "a+") as f:
     f.seek(0)
     data = f.read(1)
     if not data:
         f.write("{}")
 
+# load insformation
 with open("datos.json") as myFiles:
     Files = json.load(myFiles)
 
+# Run program
 while True:
     valor = input().split()
     if len(valor) != 2:
--- Server Files/server.py
+++ Server Files/server.py
@@ -1,20 +1,37 @@
 import zmq
 import os
 
-sizePart = 1024*1024*1024*10  # 10MB
+sizePart = 1024*1024*10  # 10MB
 context = zmq.Context()
 socket = context.socket(zmq.REP) # REP because is Reply
 socket.bind("tcp://*:3000")
 
+class trie:
+    def __init__(self):
+        self.m = {}
+    
+    def insert(self, s, idx=0) :
+        if idx < len(s) :
+            if not s[idx] in self.m :
+                self.m[s[idx]] = trie()
+            self.m[s[idx]].insert(s, idx+1)
+    
+    def search(self, s, idx=0) :
+        if idx == len(s) :
+            return True
+        if not s[idx] in self.m:
+            return False
+        return self.m[s[idx]].search(s, idx+1)
 
 def create(name) :
     name = name[1].decode()
-    with open(name, "a") as f:
-        pass
-    os.remove(name)
-    with open(name, "a") as f:
-        pass
-    socket.send(b"OK")
+    if database.search(name):
+        socket.send(b"true")
+    else :
+        database.insert(name)
+        with open(name, "a") as f:
+            pass
+        socket.send(b"false")
 
 def submit(data) :
     name = data[1].decode()
@@ -22,19 +39,19 @@ def submit(data) :
         f.write(data[2])
     socket.send(b"OK")
 
-def download(name) :
-    name = name[1].decode()
+def download(data) :
+    name = data[1].decode()
     with open(name, "rb") as f:
-        while True:
-            data = f.read(sizePart)
-            if not data:
-                socket.send(b"Finish")
-                break
+        f.seek( int( data[2].decode() ) )
+        data = f.read(sizePart)
+        if not data:
+            socket.send(b"Finish")
+        else :
             socket.send(data)
-            socket.recv()
+    print("download")
 
 switcher = {"create": create, "submit" : submit, "download": download}
-
+database = trie()
 print("Server ON")
 
 while True :
